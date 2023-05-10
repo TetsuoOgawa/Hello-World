e2d3fb8fd87f47f8501d06b6990e848d20bf4664
TetsuoOgawa <t.ogawa0927.ot@gmail.com>
2023-05-10 22:11:09 +0900

    test_2のPR

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Hello-World
 Hello-World
+## test_2
+- test_2についてのREADME
---
+++ test.java
@@ -0,0 +1,9 @@
+public class test {
+  public static void main(String[] args) {
+    System.out.println(getTest_2());
+  }
+
+  public static String getTest_2() {
+    return "test_2";
+  }
+}
