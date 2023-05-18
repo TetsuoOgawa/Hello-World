cbafbf5d5c8bcee930b9c2fb74826c71a3e87501
TetsuoOgawa <t.ogawa0927.ot@gmail.com>
2023-05-19 00:44:01 +0900

    main_20230519002

--- main.java
+++ main.java
@@ -6,5 +6,9 @@ public class main {
   public static void main_2(String[] args) {
     System.out.println("main_2");
   }
+
+  public static void main_3(String[] args) {
+    System.out.println("main_3");
+  }
   
 }
